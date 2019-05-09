2cd253c3c40715abedf0170385a52437c866f7f9
marcos <marcos.v.rodrigues@hotmail.com>
2019-05-09 18:48:29 -0300

    inicianto Forward Kinemact solver

--- body_solver.py
+++ body_solver.py
@@ -1,3 +1,6 @@
+import numpy as np
+import tinyik as ik
+
 class Body():
 	def __init__ (self):
 		self.constrants = [[1, 0, 0],
@@ -21,6 +24,24 @@ class Body():
 
 		self.CoM_parts = [[]
 						  []]
+		self.perna_dir = ik.Actuator()
 
 
 
+
+
+
+
+
+
+
+
+
+
+'''
++5.711e+0
++3.495e+1
+-6.876e+1
++3.381e+1
++5.711e+0
+'''
