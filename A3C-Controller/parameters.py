9c4286797d9fa52435dbe057b2099f92b224bfc3
marcos <marcos.v.rodrigues@hotmail.com>
2019-07-24 17:54:54 -0300

    dsa

--- A3C-Controller/parameters.py
+++ A3C-Controller/parameters.py
@@ -44,7 +44,7 @@ TIME_TO_IGNORE_GC = 0.1
 KP_CONST = 0.6
 
 #simulation
-TESTING = True
+TESTING = False
 VREP_PATH = '~/vrep'
 SCENE_FILE_PATH = '~/Documentos/controle_humanoide2018/teste_09_03.ttt'
 TIME_STEP_ACTION = 0.15
@@ -69,7 +69,7 @@ W_POSE = 5.
 OUTPUT_GRAPH = True         # safe logs
 RENDER=True                 # render one worker
 LOG_DIR = './log/weigths'   # savelocation for logs
-N_WORKERS = 1	  	# number of workers
+N_WORKERS = 20	  	# number of workers
 MAX_EP_STEP = 1000          # maxumum number of steps per episode
 MAX_EP = 1000000            # maximum number of episodes
 MAX_GLOBAL_EP = MAX_EP      # idem MAX_EP, but to tensorflow A3C implementation.
